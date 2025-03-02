179e6df6685ead3e9dac07a25dd5e031be2b7a0e
Sriram Gollapudi <gollapudiram@gmail.com>
2025-03-02 16:09:02 -0500

    changes

--- 1-terraform-jenkins-server/backend.tf
+++ 1-terraform-jenkins-server/backend.tf
@@ -1,6 +1,6 @@
 terraform {
   # backend "s3" {
-  #   bucket = "terraform-state-bkt-18022025"
+  #   bucket = "terraform-state-bkt-03022025"
   #   region = "us-east-1"
   #   key = "jenkins-server/terraform.tfstate"
   # }
--- 1-terraform-jenkins-server/jenkins-script.sh
+++ 1-terraform-jenkins-server/jenkins-script.sh
@@ -21,7 +21,7 @@ sudo yum-config-manager --add-repo https://rpm.releases.hashicorp.com/AmazonLinu
 sudo yum -y install terraform
 
 # install kubectl
-sudo curl -LO https://storage.googleapis.com/kubernetes-release/release/v1.23.6/bin/linux/amd64/kubectl
+sudo curl -LO "https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl"
 sudo chmod +x ./kubectl
 sudo mkdir -p $HOME/bin && sudo cp ./kubectl $HOME/bin/kubectl && export PATH=$PATH:$HOME/bin
 
